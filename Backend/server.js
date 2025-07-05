ab064f0d3ffa2ed61d1b4d68e0d77f088fbb1992
Suhail-Ahmed7 <suhailbhand7@gmail.com>
2025-07-05 22:18:00 +0500

    Backend Complete!

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,14 @@
 const express = require('express')
 require('dotenv').config();
 require('./config/dbConnection')()
+const cors = require('cors')
+const aiRoutes = require('./routes/aiRoutes')
 const app = express()
 app.use(express.json())
 
+app.use(cors())
+
+app.use('/ai', aiRoutes)
 const PORT = process.env.PORT || 8001;
 app.listen(PORT, () => {
     console.log(`Server running on port, ${PORT}`,);
